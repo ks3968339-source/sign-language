2741a51a9896f0e8534d7b03f10aebd328411e5e
kushal sharma <ks3968339@gmail.com>
2026-02-16 20:18:52 +0530

    fix api url

--- project/fronted/src/app/services/api.ts
+++ project/fronted/src/app/services/api.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/';
+const API_BASE_URL = "https://sign-language-r9qr.onrender.com";
+
 
 const api = axios.create({
-    baseURL: API_URL,
+    baseURL: API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
