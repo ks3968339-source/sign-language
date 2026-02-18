bd95531d27b28d8fa6d1f45dcfbfe8c88c0dac7b
kushal sharma <ks3968339@gmail.com>
2026-02-18 23:12:31 +0530

    Fix backend deploy and CORS

--- project/backend/src/controllers/authController.ts
+++ project/backend/src/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from "bcryptjs";
 import prisma from '../config/database';
 import { generateToken } from '../utils/jwt';
 import { logger } from '../utils/logger';
